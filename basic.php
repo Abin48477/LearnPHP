e614f9491ab06ed57f8153ab7ac3e5745bb6da06
Abin Ghimire <ghimireabin56@gmail.com>
2025-07-22 21:28:46 +0545

    Create basic.php

---
+++ basic.php
@@ -0,0 +1,16 @@
+<?php
+$name = "Abin";
+$marks = 90;
+
+if($marks >= 80){
+    echo " You got distiction haribol!!";
+}
+else{
+    echo " <br>You Hve to work hard.";
+}
+
+echo "<br> <br> Counting the number.";
+for($i = 0;$i<=5;$i++){
+    echo "Count $i ";
+}
+?>
