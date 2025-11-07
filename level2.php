645fb7e0d3f1278c0163151f8f6367c2eb19d23f
Abin Ghimire <ghimireabin56@gmail.com>
2025-11-07 14:06:11 +0545

    Create level2.php

---
+++ level2.php
@@ -0,0 +1,39 @@
+<?php
+/*
+//simple function
+function sayHello(){
+    echo "hello hare krishna!! <br>";
+
+}
+
+sayHello();
+sayHello();
+sayHello();
+
+// function with input(parameter)
+
+function greet($name) {
+    echo "hare krishna ". $name."!<br>";
+}
+greet("Balaram");
+greet("krishna");
+greet("abin");
+*/
+
+// function with return value
+// function add($a ,$b) {
+//     return $a + $b;
+
+// }
+// function helloFriends(){
+//     echo "jaya balaram!";
+
+// }
+// function square($n){
+//     echo " $n square is:".($n*$n) ."<br>";
+// }
+// square(4);
+
+// // echo add(13,5);
+// helloFriends();
+?>
