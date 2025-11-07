702b0447a7880095169c75b049827714fe56cfaf
Abin Ghimire <ghimireabin56@gmail.com>
2025-11-07 15:16:45 +0545

    updated

--- level3.php
+++ level3.php
@@ -1,6 +1,27 @@
 <?php
+/*
+//Types of Arrays in PHP
+1.Indexed Array(numbers as keys)
+
 $friends= ["Krishna","Balaram","arjun"];
+foreach ($friends as $f)
+{
+    echo $f ."<br>";
+}
 
-print $friends[1];
+2.Associative Ararys(words as keys)
 
+*/
+$marks =[
+    "Math" => 95,
+    "Science" => 88,
+    "English" =>90
+];
+// echo $marks["Math"];//95
+
+// 
+foreach($marks as $subject=>$m){
+    echo "$subject". ":".$m ."<br>";
+
+}
 ?>
