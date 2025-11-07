18f83c0b837d5f09f68c31a07e10bbc33d614f2b
Abin Ghimire <ghimireabin56@gmail.com>
2025-11-07 14:06:15 +0545

    Create level1.php

---
+++ level1.php
@@ -0,0 +1,58 @@
+<?php
+// echo "hello ";
+// Arithmetic Operators
+/*$a = 10;
+$b = 5;
+
+echo $a+$b ."<br>";
+echo $a-$b ."<br>";
+echo $a*$b ."<br>";
+echo $a/$b ."<br>";
+echo $a%$b ."<br>";
+
+//Assignment Operators
+$x =5;
+$x +=3;
+echo $x;
+
+//Comparision Operators==,<=,>=etc
+
+$a = 8;
+$b = 2;
+
+echo "Sum: " . ($a + $b) . "<br>";
+echo "Difference: " . ($a - $b) . "<br>";
+echo "Multiply: " . ($a * $b) . "<br>";
+echo "Divide: " . ($a / $b) . "<br>";
+echo "Remainder: " . ($a % $b) . "<br>";
+
+//if/else
+$age = 15;
+if($age >=18){
+    echo "you are adult";
+}
+else{
+    echo "you r child";
+}
+// Loops in php
+
+for($i = 1;$i <=5;$i++) {
+    echo $i ."<br>";
+}
+$i=0;
+
+while($i<=5){
+    echo $i ." hare krishna<br>";
+    $i++;
+}
+*/
+
+$a = 8;
+$b = 2;
+$sum =$a+$b;
+
+echo '$a plush $b is $sum'."<br>";//it print same 
+echo "$a plush $b is $sum";
+
+
+?>
